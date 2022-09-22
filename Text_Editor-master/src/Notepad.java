0d628254b4a4b1d4fdc314b339640becc089d476
sanket9591 <srpatil701@gmail.com>
2022-09-22 11:23:18 +0530

    final

--- Text_Editor-master/src/Notepad.java
+++ Text_Editor-master/src/Notepad.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
